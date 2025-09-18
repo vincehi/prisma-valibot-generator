7af09a4362e1bd6bc6a9ece9080619a4d774b642
souricevincent <souricevincent@icloud.com>
2025-09-18 14:26:29 +0200

    with dist lib

---
+++ lib/utils/logger.d.ts
@@ -0,0 +1,8 @@
+export declare const logger: {
+    debug: (...args: unknown[]) => void;
+    info: (...args: unknown[]) => void;
+    warn: (...args: unknown[]) => void;
+    error: (...args: unknown[]) => void;
+    isDebugEnabled: () => boolean;
+};
+export default logger;

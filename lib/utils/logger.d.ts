de5a9d2fd1692c1ae4014b2b53e81be0406c4ceb
souricevincent <souricevincent@icloud.com>
2025-09-18 14:21:23 +0200

    clean

--- lib/utils/logger.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const logger: {
-    debug: (...args: unknown[]) => void;
-    info: (...args: unknown[]) => void;
-    warn: (...args: unknown[]) => void;
-    error: (...args: unknown[]) => void;
-    isDebugEnabled: () => boolean;
-};
-export default logger;
--- lib/valibot-generator.d.ts
+++
@@ -1,2 +0,0 @@
-import type { GeneratorOptions } from "@prisma/generator-helper";
-export declare function generateValibot(options: GeneratorOptions): Promise<void>;
